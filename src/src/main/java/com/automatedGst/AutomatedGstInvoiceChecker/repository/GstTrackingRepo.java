1bb2c5ae5831f1962eb4a5f846945fa1e95bf48b
pranavraina872 <pranavraina17@gmail.com>
2026-03-24 20:12:52 +0530

    new code

--- src/src/main/java/com/automatedGst/AutomatedGstInvoiceChecker/repository/GstTrackingRepo.java
+++
@@ -1,7 +0,0 @@
-package com.automatedGst.AutomatedGstInvoiceChecker.repository;
-
-import com.automatedGst.AutomatedGstInvoiceChecker.entity.GstTracking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GstTrackingRepo extends JpaRepository<GstTracking, Long> {
-}
