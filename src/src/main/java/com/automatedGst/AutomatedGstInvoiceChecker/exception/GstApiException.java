b6934bc2946722db8f3024bb016f538039afd33c
pranavraina872 <pranavraina17@gmail.com>
2026-03-25 11:03:42 +0530

    new code

--- src/src/main/java/com/automatedGst/AutomatedGstInvoiceChecker/exception/GstApiException.java
+++
@@ -1,8 +0,0 @@
-package com.automatedGst.AutomatedGstInvoiceChecker.exception;
-
-public class GstApiException extends RuntimeException {
-
-    public GstApiException(String message) {
-        super(message);
-    }
-}
